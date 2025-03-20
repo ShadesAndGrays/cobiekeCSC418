29c037175a589c83302dc679670e03390685d763
ShadesAndGrays <burner00shadow@gmail.com>
2025-03-20 23:29:25 +0100

    completed project 2

--- week-2/project2.py
+++ week-2/project2.py
@@ -8,17 +8,40 @@ class_directory = "img/class" # all profiles are stored here
 
 class ImageFilter:
     @staticmethod
+    def prompt():
+        choice = input("""
+Select a filter:
+1. gray_scale
+2. invert
+3. blur
+4. sharpen
+5. contrast and brightness
+    """)
+        match choice := int(choice):
+            case 1:
+                return ImageFilter.gray_scale
+            case 2:
+                return ImageFilter.invert
+            case 3:
+                return ImageFilter.median_blur
+            case 4:
+                return ImageFilter.sharpen_image
+            case 5:
+                return ImageFilter.contrast
+
+    @staticmethod
     def gray_scale(image):
-        B, G , R = cv2.split(image)
-        return R
+        _, _ , out = cv2.split(image)  # bgr
+        return out # only take the red channel 
 
     @staticmethod
-    def invert(image):
-        return 255 - image
+    def invert(image,threshold=255):
+        out =  threshold - image
+        return out
 
     @staticmethod
-    def median_blur(image):
-        out = cv2.medianBlur(image,15)
+    def median_blur(image,blur_amount=15):
+        out = cv2.medianBlur(image,blur_amount)
         return out
 
     @staticmethod
@@ -28,16 +51,29 @@ class ImageFilter:
         return out
 
     @staticmethod
-    def contrast(image):
-        brightness = 5
-        contrast = 1.5
+    def contrast(image,brightness = 5,contrast = 1.5):
         out = cv2.addWeighted(image,contrast,np.zeros(image.shape,image.dtype),0,brightness)
         return out
 
 def main():
-    img = cv2.imread("img/grassland.jpg")
-    cv2.imshow("File",ImageFilter.median_blur(img))
-    cv2.waitKey(10000) 
-    pass
+    print("Welcome to stutent effects")
+    username = input("input username: ")
+    matno = input("input mat no: ")
+    path = f"{root_dir}/{class_directory}/{username}.jpeg" 
+    if os.path.exists(path):
+        img = cv2.imread(path)
+        filter = ImageFilter.prompt()
+        if filter == None:
+            return
+        img = filter(img)
+        output_path = f"{root_dir}/{class_directory}/{username}_render.jpeg"
+        cv2.imwrite(output_path,img)
+        print(output_path,"generated")
+        cv2.imshow(f"{username}: {matno}",img)
+        cv2.waitKey(0)
+
+    else:
+        print("user does not exist")
+        pass
 
 main()
